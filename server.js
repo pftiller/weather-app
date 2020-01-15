26acbf03a079997b816b1788ff744d3fa0eabd66
Paul Tiller <pftiller@gmail.com>
2020-01-15 14:51:19 -0600

    adding comments, removing logging

--- server.js
+++ server.js
@@ -1,38 +1,35 @@
-
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
-
+// Express to run server and routes
 const express = require('express');
+// Start up an instance of app
 const app = express();
-const bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+/* Dependencies */
 const cors = require('cors');
+/* Middleware*/
+const bodyParser = require('body-parser')
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+// Cors for cross origin allowance
 app.use(cors());
 const port = 3000;
-
 // Initialize the main project folder
 app.use(express.static('website'));
-
-
-// Setup Server
-app.listen(port, ()=>{
+// Setup Server and callback to debug
+app.listen(port, () => {
     console.log(`server is up and running on port ${port}`);
 })
-
-
-
 // GET Route
-app.get('/weather', (req, res)=>{
+app.get('/all', (req, res) => {
     res.send(projectData);
 })
-
 // POST Route
-app.post('/add', (req, res)=>{
-    console.log(req.body)
+app.post('/add', (req, res) => {
     projectData.temp = req.body.temp;
     projectData.date = req.body.date;
     projectData.content = req.body.content;
-    console.log(projectData);
     res.send(projectData);
 })
--- website/app.js
+++ website/app.js
@@ -1,12 +1,12 @@
 /* Global Variables */
+// Personal API Key for OpenWeatherMap API
 const APPID = "8dfdc3b3ea6ea099e2582bd58091f226";
 const baseURI = "http://api.openweathermap.org/data/2.5/weather"
 // Create a new date instance dynamically with JS
 let d = new Date();
-let date = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
-
-
-const handleClick = async ()=> {
+let date = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+/* Function called by event listener */
+const handleClick = async () => {
     let content = document.getElementById('feelings').value;
     let temp = await getWeather();
     try {
@@ -15,6 +15,7 @@ const handleClick = async ()=> {
         console.log(err)
     }
 }
+/* Function to GET Web API Data*/
 const getWeather = async () => {
     let zip = document.getElementById('zip').value;
     const response = await fetch(`${baseURI}?zip=${zip},us&units=imperial&APPID=${APPID}`, {
@@ -27,6 +28,7 @@ const getWeather = async () => {
         console.log(err)
     }
 }
+/* Function to POST data */
 const postWeather = async (temp, content, date) => {
     let dataToPost = {
         temp: temp,
@@ -40,31 +42,26 @@ const postWeather = async (temp, content, date) => {
         },
         body: JSON.stringify(dataToPost)
     }).then((res) => {
-        fetch('/weather', {
+        /* GET Project Data */
+        fetch('/all', {
             method: 'GET',
         })
         return res.json();
     })
     try {
-        console.log(finalResponse);
         let entryHolder = document.getElementById('entryHolder');
-        console.log(entryHolder)
-        console.log(entryHolder)
-        if(entryHolder.firstElementChild == null) {
-
-
+        if (entryHolder.firstElementChild == null) {
             let text = "<table><tr><th>Date</th><th>Temperature</th><th>Mood</th></tr>"
             text = text + `<tr><td>${finalResponse.date}</td><td>${finalResponse.temp} </td><td>${finalResponse.content}</td></tr>`
             entryHolder.innerHTML = entryHolder.innerHTML + text;
-        }
-        else {
+        } else {
             let theTable = document.querySelector('table');
             let text = `<tr><td>${finalResponse.date}</td><td>${finalResponse.temp} </td><td>${finalResponse.content}</td></tr>`;
             theTable.innerHTML = theTable.innerHTML + text;
         }
-
     } catch (err) {
         console.log(err)
     }
 }
+// Event listener to add function to existing HTML DOM element
 document.getElementById('generate').addEventListener('click', handleClick);
