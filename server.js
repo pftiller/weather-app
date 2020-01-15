68436d1ceea9a100cb6e07df37b164143d9ac136
Paul Tiller <pftiller@gmail.com>
2020-01-15 12:56:40 -0600

    basic functionality

--- server.js
+++ server.js
@@ -1,19 +1,38 @@
+
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
-// Require Express to run server and routes
-
-// Start up an instance of app
-
-/* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+const port = 3000;
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+app.listen(port, ()=>{
+    console.log(`server is up and running on port ${port}`);
+})
+
+
+
+// GET Route
+app.get('/weather', (req, res)=>{
+    res.send(projectData);
+})
+
+// POST Route
+app.post('/add', (req, res)=>{
+    console.log(req.body)
+    projectData.temp = req.body.temp;
+    projectData.date = req.body.date;
+    projectData.content = req.body.content;
+    console.log(projectData);
+    res.send(projectData);
+})
--- website/app.js
+++ website/app.js
@@ -1,5 +1,54 @@
 /* Global Variables */
-
+const APPID = "8dfdc3b3ea6ea099e2582bd58091f226";
+const baseURI = "http://api.openweathermap.org/data/2.5/weather"
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let date = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+
+const handleClick = async ()=> {
+    let content = document.getElementById('feelings').value;
+    let temp = await getWeather();
+    try {
+        postWeather(temp, content, date);
+    } catch (err) {
+        console.log(err)
+    }
+}
+const getWeather = async () => {
+    let zip = document.getElementById('zip').value;
+    const response = await fetch(`${baseURI}?zip=${zip},us&units=imperial&APPID=${APPID}`, {
+        method: 'GET',
+    })
+    try {
+        const newData = await response.json();
+        return newData.main.temp;
+    } catch (err) {
+        console.log(err)
+    }
+}
+const postWeather = async (temp, content, date) => {
+    let dataToPost = {
+        temp: temp,
+        content: content,
+        date: date
+    }
+    const finalResponse = await fetch('/add', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(dataToPost)
+    }).then((res) => {
+        fetch('/weather', {
+            method: 'GET',
+        })
+        return res.json();
+    })
+    try {
+        console.log(finalResponse);
+    } catch (err) {
+        console.log(err)
+    }
+}
+document.getElementById('generate').addEventListener('click', handleClick);
